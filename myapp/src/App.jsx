34441568a5bd48b82f1d43595ed6e8feca905a87
raghuveerdsunagar-prog <raghuveerdsunagar@gmail.com>
2026-02-23 16:35:40 +0530

    Fix import path for Dashboard component

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StatsGrid from "./StatsGrid";
-import Dashboard from "./dashboard";
+import Dashboard from "./Dashboard";
 import Adminlogin from "./Admin";
 function App() {
   return (
